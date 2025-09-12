fd1eb167c9ec25c77ff4e165644dcdc1cf3c76dc
Dishant Sangani <128277319+Dishantsangani@users.noreply.github.com>
2025-09-12 11:55:00 +0530

    Update productApi.js

--- Frontend/src/API/Web/productApi.js
+++ Frontend/src/API/Web/productApi.js
@@ -1,10 +1,7 @@
 import axios from "axios";
 import { Toastifyerror } from "../../Component/Notification/Toastitynotificaition";
 
-// const BASE_URL = `${import.meta.env.VITE_BACKEND_PORT}/base/web`;
-
-const BASE_URL = `https://digital-den-n5yv.onrender.com/base/web`;
-
+const BASE_URL = `${import.meta.env.VITE_BACKEND_PORT}/base/web`;
 export const getProductWebApi = async () => {
   try {
     const response = await axios.get(`${BASE_URL}/getproduct`);
