7a83a87eddade100fd0cb4803b4b19b38e5c05d6
Dishant <devsangani168@gmail.com>
2026-01-28 10:56:41 +0530

    feat: Initialize backend project with Express, TypeScript, and Vercel deployment configuration.

---
+++ Backend/api/index.ts
@@ -0,0 +1,41 @@
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import config from "../src/utils/config/config.js";
+import baseRouter from "../src/routes.js";
+import cookieParser from "cookie-parser";
+
+dotenv.config();
+const app = express();
+
+const port = process.env.PORT || config.port;
+
+const allowsOrigins = [config.Deploy_frontend_url, config.frontend_url];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin || allowsOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  }),
+);
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+
+app.use(baseRouter);
+
+app.get("/", (req, res) => {
+  res.send(`<h1> Welcome to DigitalDen Server!</h1>
+    <p>Backend is running successfully.</p>`);
+});
+
+export default app;
--- Backend/index.ts
+++ Backend/index.ts
@@ -1,43 +1,9 @@
+import app from "./api/index";
 import dotenv from "dotenv";
-import express from "express";
-import cors from "cors";
-import config from "./src/utils/config/config.js";
-import baseRouter from "./src/routes.js";
-import cookieParser from "cookie-parser";
-
 dotenv.config();
-const app = express();
 
-const port = process.env.PORT || config.port;
+const port = process.env.PORT || 8080;
 
-const allowsOrigins = [config.Deploy_frontend_url, config.frontend_url];
-
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin || allowsOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-  }),
-);
-
-app.use(express.json());
-
-app.use(express.urlencoded({ extended: true }));
-
-app.use(cookieParser());
-
-app.use(baseRouter);
-
-app.get("/", (req, res) => {
-  res.send(`<h1> Welcome to DigitalDen Server!</h1>
-    <p>Backend is running successfully.</p>`);
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
 });
-
-app.listen(port, () =>
-  console.log(`Server Started At Port http://localhost:${port}`),
-);
